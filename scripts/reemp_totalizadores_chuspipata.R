6cc522b2fcba1771f409335687c231d7e74d7494
noriegaedu <eduardonoriegac@gmail.com>
2018-02-01 19:17:08 -0400

    no correr para evitar sobrescritura

--- scripts/reemp_totalizadores_chuspipata.R
+++ scripts/reemp_totalizadores_chuspipata.R
@@ -6,5 +6,6 @@ reempl = read.table("C:/Users/HP/Desktop/EST/desde 1981/chuspipata_totalizadores
 
 est_mes[,names(reempl[2])] = reempl[2]
 
-write.csv(est_mes, "H:/@Documentos/Tesis/ProyR_T_CH/data/lluvias_2017/resumidos/est_mes.csv", row.names=F)
+# write.csv(est_mes, "H:/@Documentos/Tesis/ProyR_T_CH/data/lluvias_2017/resumidos/est_mes.csv", row.names=F)
+# no correr para evitar sobrescritura
 
